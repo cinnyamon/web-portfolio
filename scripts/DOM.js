fd031d12f50af7bee4901958f47decdc7a3d5449
cinny <cinnyamon@disroot.org>
2025-08-26 10:19:24 +0300

    Small improvements.

---
+++ scripts/DOM.js
@@ -0,0 +1,138 @@
+export const createDOMAndEventHandlers = (fullImageDataObj, mainEl, imageOpened) => {
+  // image viewer 
+  const fullSizeImgContainer = document.createElement('div');
+
+  // actual full size img
+  const fullSizeImgWrapper = document.createElement('div');
+  const fullSizeImgEl = document.createElement('img');
+  
+
+  // create post data elements
+
+
+  // post info container
+  const postInfoEl = document.createElement('div');
+
+  // post title
+  const postTitle = document.createElement('h2');
+
+  // post likes and tags container
+  const postTagsLikesCont = document.createElement('div');
+
+  // post likes and icon
+  const postNoteCountBtn = document.createElement('button');
+  const postNoteHeart = document.createElement('img');
+  const postNoteCount = document.createElement('p');
+
+  // post tags 
+  const postTagsDiv = document.createElement('div');
+
+  // post links
+  const postLinkDiv = document.createElement('div')
+  const postLink = document.createElement('a');
+
+
+
+
+  // image and info container 
+  fullSizeImgContainer.classList.add('full-size-img-container');
+
+  // full size image 
+  fullSizeImgWrapper.classList.add('full-size-img-wrapper');
+  fullSizeImgEl.src = fullImageDataObj.highReslinks;
+  fullSizeImgEl.classList.add('full-size-image')
+
+  // post info container
+  postInfoEl.classList.add('post-info-container');
+
+  // post title
+  postTitle.textContent = `[ ${fullImageDataObj.title} ]`;
+  postTitle.classList.add('post-title');
+
+  // post tags and likes container
+  postTagsLikesCont.classList.add('likes-tags-container');
+
+  // post likes and icon
+  postNoteHeart.src = '../assets/pixel-heart.svg'
+  postNoteHeart.classList.add('post-heart-icon');
+  postNoteCount.innerHTML = fullImageDataObj.notes;
+  postNoteCountBtn.classList.add('post-notes-btn');
+
+  // post tags
+  postTagsDiv.classList.add('post-tags-div');
+  for (let x of fullImageDataObj.tags) {
+    console.log(x)
+    const postTags = document.createElement('p');
+    postTags.textContent = x;
+    postTagsDiv.append(postTags);
+  }
+
+  // post original link
+  postLink.href = fullImageDataObj.url;
+  postLink.textContent = 'Original Post';
+  postLink.classList.add('post-link-text');
+  postLinkDiv.classList.add('post-link-div');
+
+
+  // append elements to respective divs
+  postNoteCountBtn.append(postNoteHeart, postNoteCount);
+  postTagsLikesCont.append(postNoteCountBtn, postTagsDiv);
+  postLinkDiv.append(postLink);
+  postInfoEl.append(
+    postTitle,
+    postTagsLikesCont,
+    postLinkDiv,
+  )
+
+  fullSizeImgWrapper.append(
+    fullSizeImgEl,
+  )
+
+  fullSizeImgContainer.append(
+    fullSizeImgWrapper,
+    postInfoEl
+  )
+  mainEl.append(fullSizeImgContainer);
+
+
+
+  let zoomedInto = false
+
+  handleContainerFunctionality(imageOpened, zoomedInto, fullSizeImgContainer, fullSizeImgEl, postNoteCountBtn, fullImageDataObj)
+}
+
+const handleContainerFunctionality = (imageOpened, zoomedInto, fullSizeImgContainer, fullSizeImgEl, postNoteCountBtn, fullImageDataObj) => {
+
+  // handle clicking outside the image to close it.
+  fullSizeImgContainer.addEventListener('click', (e) => {
+    if (!fullSizeImgEl.contains(e.target)) {
+      fullSizeImgContainer.remove()
+      imageOpened = false
+    }
+  })
+
+  // handle zooming into the full size image 
+  fullSizeImgEl.addEventListener('click', (e) => {
+    if (!zoomedInto) {
+      const fullSizeImgRect = fullSizeImgEl.getBoundingClientRect();
+      const offSetX = e.clientX - fullSizeImgRect.left
+      const offSetY = e.clientY - fullSizeImgRect.top
+      const percentX = (offSetX / fullSizeImgRect.width) * 100;
+      const percentY = (offSetY / fullSizeImgRect.height) * 100;
+
+      fullSizeImgEl.style.transformOrigin = `${percentX}% ${percentY}%`;
+      fullSizeImgEl.style.transform = 'scale(2)';
+      fullSizeImgEl.classList.add('zoomed-in');
+      zoomedInto = true;
+    } else {
+      fullSizeImgEl.style.transform = 'scale(1)';
+      fullSizeImgEl.classList.remove('zoomed-in');
+      zoomedInto = false;
+    }
+  })
+
+  // like button functionality
+  postNoteCountBtn.addEventListener('click', (e) => {
+    location.href = fullImageDataObj.url
+  })
+}
--- scripts/imageFetching.js
+++ scripts/imageFetching.js
@@ -1,4 +1,5 @@
 import { key } from "./apiKey.js";
+import { createDOMAndEventHandlers } from "./DOM.js";
 
 
 export const galleryFetching = () => {
@@ -144,6 +145,7 @@ export const galleryFetching = () => {
     return { fullImageData, lowResData }
   }
 
+  
 
   const insertImages = async () => {
     // wait to fetch all images
@@ -177,6 +179,7 @@ export const galleryFetching = () => {
 
   }
 
+
   const placeImagesInPage = (entry) => {
     let imageOpened = false;
     const imagEl = document.createElement('img');
@@ -192,6 +195,7 @@ export const galleryFetching = () => {
     })
   }
 
+
   const handleClickingImage = async (imagEl, imageOpened) => {
     const fullImageDataObj = await fetchImgById(imagEl.dataset.id);
 
@@ -200,133 +204,14 @@ export const galleryFetching = () => {
     if(imagEl.dataset.id !== fullImageDataObj.id) return; 
     
 
-    
+    // open the full window container
     if (!imageOpened) {
       imageOpened = true;
 
-
-
-      // image viewer 
-      const fullSizeImgContainer = document.createElement('div');
-  
-      // actual full size img
-      const fullSizeImgWrapper = document.createElement('div');
-      const fullSizeImgEl = document.createElement('img');
-      
-
-      // create post data elements
-
-
-      // post info container
-      const postInfoEl = document.createElement('div');
-
-      // post title
-      const postTitle = document.createElement('h2');
-
-      // post likes and icon
-      const postNoteCountBtn = document.createElement('button');
-      const postNoteHeart = document.createElement('img');
-      const postNoteCount = document.createElement('p');
-
-      // post tags 
-      const postTagsDiv = document.createElement('div');
-
-      // post links
-      const postLinkDiv = document.createElement('div')
-      const postLink = document.createElement('a');
-
-
-
-
-      // image and info container 
-      fullSizeImgContainer.classList.add('full-size-img-container');
-
-      // full size image 
-      fullSizeImgWrapper.classList.add('full-size-img-wrapper');
-      fullSizeImgEl.src = fullImageDataObj.highReslinks;
-      fullSizeImgEl.classList.add('full-size-image')
-
-      // post info container
-      postInfoEl.classList.add('post-info-container');
-
-      // post title
-      postTitle.textContent = `[ ${fullImageDataObj.title} ]`;
-      postTitle.classList.add('post-title');
-
-      // post likes and icon
-      postNoteHeart.src = '../assets/pixel-heart.svg'
-      postNoteHeart.classList.add('post-heart-icon');
-      postNoteCount.innerHTML = fullImageDataObj.notes;
-      postNoteCountBtn.classList.add('post-notes-btn');
-
-      // post tags
-      postTagsDiv.classList.add('post-tags-div');
-      for (let x of fullImageDataObj.tags) {
-        console.log(x)
-        const postTags = document.createElement('p');
-        postTags.textContent = x;
-        postTagsDiv.append(postTags);
-      }
-
-      // post original link
-      postLink.href = fullImageDataObj.url;
-      postLink.textContent = 'Original Post';
-      postLink.classList.add('post-link-text');
-      postLinkDiv.classList.add('post-link-div');
-
-
-      // append elements to respective divs
-      postNoteCountBtn.append(postNoteHeart, postNoteCount);
-      postLinkDiv.append(postLink);
-      postInfoEl.append(
-        postTitle,
-        postNoteCountBtn,
-        postTagsDiv,
-        postLinkDiv,
-      )
-
-      fullSizeImgWrapper.append(
-        fullSizeImgEl,
-      )
-
-      fullSizeImgContainer.append(
-        fullSizeImgWrapper,
-        postInfoEl
-      )
-      mainEl.append(fullSizeImgContainer);
-
-
-
-      // handle clicking outside the image to close it.
-      fullSizeImgContainer.addEventListener('click', (e) => {
-        if (!fullSizeImgEl.contains(e.target)) {
-          fullSizeImgContainer.remove()
-          imageOpened = false
-        }
-      })
-
-      // handle zooming into the full size image 
-      let zoomedInto = false
-      fullSizeImgEl.addEventListener('click', (e) => {
-        if (!zoomedInto) {
-          const fullSizeImgRect = fullSizeImgEl.getBoundingClientRect();
-          const offSetX = e.clientX - fullSizeImgRect.left
-          const offSetY = e.clientY - fullSizeImgRect.top
-          const percentX = (offSetX / fullSizeImgRect.width) * 100;
-          const percentY = (offSetY / fullSizeImgRect.height) * 100;
-
-          fullSizeImgEl.style.transformOrigin = `${percentX}% ${percentY}%`;
-          fullSizeImgEl.style.transform = 'scale(2)';
-          fullSizeImgEl.classList.add('zoomed-in');
-          zoomedInto = true;
-        } else {
-          fullSizeImgEl.style.transform = 'scale(1)';
-          fullSizeImgEl.classList.remove('zoomed-in');
-          zoomedInto = false;
-        }
-      })
+      createDOMAndEventHandlers(fullImageDataObj, mainEl, imageOpened)
     }
   }
+
 
   const fetchImgById = async (id) => {
     return new Promise((resolve, reject) => {
